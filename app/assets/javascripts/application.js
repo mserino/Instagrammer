9c82258ad028719c0c5ec419b8cdfdb6b9e9ff21
mserino <m.serino@hotmail.it>
2014-08-09 16:12:32 +0200

    Tagging feature - Can click on single tag and shows all the posts related

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,4 @@
 //= require bootstrap-sprockets
 //= require_tree .
 //= require jquery
+//= require bootstrap-tagsinput
--- spec/features/tags_feature_spec.rb
+++ spec/features/tags_feature_spec.rb
@@ -15,4 +15,25 @@ describe 'Tagging posts' do
 			expect(page).to have_content '#swag'
 		end
 	end
+
+	context 'existing posts' do 
+
+		before do
+			user = User.create email: 'alex@example.com', password: '12345678', password_confirmation: '12345678'
+			user.posts.create title: 'Post1', tag_names: 'yolo'
+			user.posts.create title: 'Post2', tag_names: 'swag'
+			visit '/posts'
+		end
+
+		it 'should filter posts by selected tag' do
+			click_link 'yolo'
+			expect(page).to have_content 'Post1'
+			expect(page).not_to have_content 'Post2'
+		end
+
+		it 'uses the tag name in the url' do
+			click_link 'yolo'
+			expect(current_path).to eq '/tags/yolo'
+		end
+	end
 end
